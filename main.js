935160508425b073453323ed46fc987d95e15814
SneedZz <florian.zado1@gmx.de>
2025-02-16 18:55:56 +0100

    Bücher hinzufügen

--- main.js
+++ main.js
@@ -11,7 +11,27 @@ function Book(title, author, pages, read) {
 }
 
 const Book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
+addBookToLibrary(Book1.title, Book1.author, Book1.pages, Book1.read);
 
-function addBookToLibrary() {}
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary[title] = newBook;
+}
 
-console.log(Book1.info());
+function bookDisplay() {
+  const books = Object.values(myLibrary);
+  books.forEach((book) => {
+    console.log(book.info());
+  });
+}
+
+document.getElementById('submit').addEventListener('click', (event) => {
+  event.preventDefault(); //Standard verhalten des submits unterdrücken
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = document.getElementById('read').value;
+  addBookToLibrary(title, author, pages, read);
+  bookDisplay();
+});
+console.log(myLibrary);
